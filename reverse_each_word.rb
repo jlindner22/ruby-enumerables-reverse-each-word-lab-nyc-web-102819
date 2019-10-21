87c857d1036eedca3882dbf4ce62a345f0bdfd45
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 17:34:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,15 @@
 def reverse_each_word
-  word.each do |word| 
-puts word
-  
+#   word.each do |word| 
+# puts word
+ def reverse(string)
+ string = string.split('')
+ reversed_string = []
+ 
+ string.each do |char|
+  reversed_string.unshift(char)
+ end
+ 
+ return reversed_string.join('')
+end 
   
 end
