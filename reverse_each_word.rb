763dae027e4b07dd5bef9e2e6c20544ad1a882d7
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 18:00:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,13 +4,10 @@
 # final = reversed_string.join('')
 # return final
 # end 
-  
 
-#   word.each do |word| 
-# puts word
 
 def reverse_each_word(array)
   string_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
+  reversed_array = string_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
 end
