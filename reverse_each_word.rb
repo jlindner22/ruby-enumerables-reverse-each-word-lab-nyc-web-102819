8437d7a9ba6d9b6c260c5a4e1d5a429b487aca72
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 18:03:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,3 @@
-# def reverse_each_word(string)
-# words = string.split('')
-# reversed_string = words.each {|x| x.reverse!}
-# final = reversed_string.join('')
-# return final
-# end 
-
-
 def reverse_each_word(string)
-  string_array = string.split(" ")
-  reversed_array = string_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  string.split(" ").collect {|x| x.reverse!}.join(" ")
 end
