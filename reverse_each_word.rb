f8eaf340dc46921b40ab3330867fc74b10b9bc6a
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 17:37:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
  reversed_string = []
  
  string.each do |char|
-  reversed_string.unshift(char)
+  reversed_string.push(char)
  end
  
  return reversed_string.join('')
